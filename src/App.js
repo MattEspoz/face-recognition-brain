510b9ba54fb2239f422ed21e198955695b4ea722
Matthew Espinoza <matthewespinoza02@gmail.com>
2022-05-30 12:03:33 -0400

    Updating to include particles in the background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,116 @@
 import React, {Component} from 'react';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImagesLinkedForm from './components/ImagesLinkedForm/ImagesLinkedForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+// const particlesOptions = {
+//   particles: {
+//     line_linked: {
+//       shadow: {
+//         enable: true,
+//         color: "#3CA9D1",
+//         blur: 5
+//       }
+//     }
+//   }
+// }
+
 function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+
   return (
     <div className="App">
+      <Particles className = 'particles'
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 60,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: false,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 4,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 40,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
       <Navigation/>
       <Logo />
+      <Rank />
       <ImagesLinkedForm />
       {/* <FaceRecognition />
       } */}
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -7,7 +7,7 @@ import brain from './brain.png'
 const Logo = () => {
     return (
        <div className = 'ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 150 }} style={{ height: 100, width: 100 }} >
             <div className="Tilt-inner pa3"> 
               <img style={{padingTop: '5px'}} alt='logo' src ={brain}/>
              </div>
