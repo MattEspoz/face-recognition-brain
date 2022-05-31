900945212f4cf00d68053b23737f1c61dff7834f
Matthew Espinoza <matthewespinoza02@gmail.com>
2022-05-31 09:12:41 -0400

    Fully functional App (without AI added)

--- src/App.js
+++ src/App.js
@@ -7,125 +7,106 @@ import ImagesLinkedForm from './components/ImagesLinkedForm/ImagesLinkedForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-// const particlesOptions = {
-//   particles: {
-//     line_linked: {
-//       shadow: {
-//         enable: true,
-//         color: "#3CA9D1",
-//         blur: 5
-//       }
-//     }
-//   }
-// }
+// setting up Particles NPM
+const particlesInit = async (main) => {
+  console.log(main);
 
-function App() {
-  constructor() {
-    super();
-    this.state = {
-      input: '',
-    }
-  }
+  // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+  // starting from v2 you can add only the features you need reducing the bundle size
+  await loadFull(main);
+};
 
-  onInputChange = (event) => {
-    console.log(event.target.value);
-  }
+const particlesLoaded = (container) => {
+  console.log(container);
+};
 
-  onSubmit = () => {
-    console.log('click');
-  }
-
-  const particlesInit = async (main) => {
-    console.log(main);
-    await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-  return (
-    <div className="App">
-      <Particles className = 'particles'
-        id="tsparticles"
-        init={particlesInit}
-        loaded={particlesLoaded}
-        options={{
-          fpsLimit: 60,
-          interactivity: {
-            events: {
-              onClick: {
+// Rendering App
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className = 'particles'
+          id="tsparticles"
+          init={particlesInit}
+          loaded={particlesLoaded}
+          options={{
+            fpsLimit: 60,
+            interactivity: {
+              events: {
+                onClick: {
+                  enable: true,
+                  mode: "push",
+                },
+                onHover: {
+                  enable: true,
+                  mode: "repulse",
+                },
+                resize: true,
+              },
+              modes: {
+                push: {
+                  quantity: 4,
+                },
+                repulse: {
+                  distance: 200,
+                  duration: 0.4,
+                },
+              },
+            },
+            particles: {
+              color: {
+                value: "#ffffff",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
                 enable: true,
-                mode: "push",
+                opacity: 0.5,
+                width: 1,
               },
-              onHover: {
+              collisions: {
+                enable: false,
+              },
+              move: {
+                direction: "none",
                 enable: true,
-                mode: "repulse",
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 4,
+                straight: false,
               },
-              resize: true,
-            },
-            modes: {
-              push: {
-                quantity: 4,
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 40,
               },
-              repulse: {
-                distance: 200,
-                duration: 0.4,
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
               },
             },
-          },
-          particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#ffffff",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
-            collisions: {
-              enable: false,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 4,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 40,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 1, max: 5 },
-            },
-          },
-          detectRetina: true,
-        }}
-      />
-      <Navigation/>
-      <Logo />
-      <Rank />
-      <ImagesLinkedForm onInputChange={this.onInputChange}/>
-      {/* <FaceRecognition />
-      } */}
-    </div> 
-  );
+            detectRetina: true,
+          }}
+        />
+        <Navigation/>
+        <Logo />
+        <Rank />
+        <ImagesLinkedForm onInputChange={this.onInputChange}/>
+        {/* <FaceRecognition />
+        */}
+      </div> 
+    );
+  }
 }
 
 export default App;
